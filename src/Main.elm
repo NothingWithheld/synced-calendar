1abf0694ffc7ea5b07ded1785ed80cdd82a03cc9
unknown <nothingwithheldsocial@gmail.com>
2020-02-26 16:13:34 -0600

    Add box shadows

--- src/Main.elm
+++ src/Main.elm
@@ -481,7 +481,7 @@ viewSingleDayTimeSlots model dayNum =
 viewTimeSlot : Model -> Int -> Int -> Lists.ListItem Msg
 viewTimeSlot _ dayNum slotNum =
     Lists.li
-        [ css "border" "thin solid black"
+        [ css "border" "thin solid #829AB1"
         , Options.onMouseDown (StartSelectingTimeSlot dayNum slotNum)
         ]
         []
@@ -524,8 +524,10 @@ viewCurrentlySelectingTimeSlot model dayNum =
                     , css "top" (String.fromFloat cardDimensions.y ++ "px")
                     , css "height" (String.fromFloat cardDimensions.height ++ "px")
                     , css "position" "absolute"
-                    , css "width" "100%"
+                    , css "width" "95%"
                     , css "z-index" "4"
+                    , css "box-shadow" "0 6px 10px 0 rgba(0,0,0,0.14), 0 1px 18px 0 rgba(0,0,0,0.12), 0 3px 5px -1px rgba(0,0,0,0.2)"
+                    , css "border-radius" "8px"
                     ]
                     [ styled div [ Typography.subheading1 ] [ text "(group name)" ] ]
 
@@ -558,6 +560,7 @@ viewUserRequestForm model eventCreationDetails eventCreationPosition =
         , css "left" (String.fromFloat eventCreationPosition.x ++ "px")
         , css "top" (String.fromFloat eventCreationPosition.y ++ "px")
         , css "padding" "12px 8px 0px"
+        , css "box-shadow" "0 24px 38px 3px rgba(0,0,0,0.14), 0 9px 46px 8px rgba(0,0,0,0.12), 0 11px 15px -7px rgba(0,0,0,0.2)"
         , Options.onWithOptions "click"
             (Decode.succeed
                 { message = NoOp
