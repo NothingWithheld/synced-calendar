84c916b61f40f13e15a732109b3f3987d6793072
Annie Qiu <annieq2@illinois.edu>
2020-03-15 21:28:49 -0500

    Add DELETE request for FreeTimeEntry

--- server/src/Handler/TimeEntry.hs
+++ server/src/Handler/TimeEntry.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RecordWildCards     #-}
 
 module Handler.TimeEntry where 
 
 import Import 
+import Database.Persist.Sql (toSqlKey)
 import Data.Time.LocalTime
 import Data.Text.Read
 import qualified Data.Text as T
+
+newtype Key entity = Key { unKey :: PersistValue }
 
 getFreeTimeEntryR :: Text -> Handler Value 
 getFreeTimeEntryR userId = do 
@@ -25,6 +29,17 @@ postFreeTimeEntryR userId = do
             insertedFreeTimeEntry <- runDB $ insertEntity timeEntry'
             returnJson insertedFreeTimeEntry
         (_, _, _) -> invalidArgs ["Failed to parse day and from_time and to_time params"]
+
+deleteFreeTimeEntryR :: Text -> Handler Value 
+deleteFreeTimeEntryR entryIdText = do
+    -- runDB $ delete (Key entryIdText)
+    -- return Null
+    let eitherEntryId = decimal entryIdText 
+    case eitherEntryId of 
+        Right (entryIdInt, "") -> do
+            runDB $ deleteWhere [FreeTimeEntryId ==. toSqlKey (fromIntegral (entryIdInt::Integer))]
+            return Null
+        _ -> badMethod
 
 convertTextToTime :: Maybe Text -> Maybe TimeOfDay 
 convertTextToTime Nothing = Nothing
