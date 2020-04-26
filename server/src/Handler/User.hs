6607ad74a88630af541dbfdcc00bf8487bfafcb3
Annie Qiu <annieq2@illinois.edu>
2020-04-26 15:35:26 -0500

    Make password salt for user

--- server/src/Handler/User.hs
+++ server/src/Handler/User.hs
@@ -4,19 +4,27 @@
 module Handler.User where 
 
 import Import
+import Yesod.Auth.Util.PasswordStore
+
+strength :: Int
+strength = 12
+
+salt :: ByteString
+salt = "3s18bst5ebf"
 
 postUserR :: Handler Value 
 postUserR = do 
     maybeEmail <- lookupPostParam "email"
-    maybeHashedPass <- lookupPostParam "hashed_pass"
-    case (maybeEmail, maybeHashedPass) of 
-        (Just email, Just hashedPass) -> do 
+    maybePassword <- lookupPostParam "password"
+    case (maybeEmail, maybePassword) of 
+        (Just email, Just password) -> do 
             potentialUsers <- runDB $ selectList [UserEmail ==. email] []
             case potentialUsers of 
                 [_] -> invalidArgs ["User is already created with email: " Import.++ email]
                 _ -> do 
                     let registered = True
-                    let user' = User email hashedPass registered
+                    let hashedPass = makePasswordSalt (Import.encodeUtf8 password) (makeSalt salt) strength
+                    let user' = User email (Import.decodeUtf8 hashedPass) registered
                     insertedUser <- runDB $ insertEntity user' 
                     returnJson insertedUser
         (_, _) -> invalidArgs ["Failed to provide email and/or hashed_pass values"]
@@ -24,10 +32,11 @@ postUserR = do
 getUserR :: Handler Value 
 getUserR = do 
     maybeEmail <- lookupPostParam "email"
-    maybeHashedPass <- lookupPostParam "hashed_pass"
-    case (maybeEmail, maybeHashedPass) of 
-        (Just email, Just hashedPass) -> do 
-            potentialUsers <- runDB $ selectList [UserEmail ==. email, UserHashedPass ==. hashedPass] []
+    maybePassword <- lookupPostParam "password"
+    case (maybeEmail, maybePassword) of 
+        (Just email, Just password) -> do 
+            let hashedPass = makePasswordSalt (Import.encodeUtf8 password) (makeSalt salt) strength
+            potentialUsers <- runDB $ selectList [UserEmail ==. email, UserPassword ==. (Import.decodeUtf8 hashedPass)] []
             case potentialUsers of 
                 [user] -> returnJson user
                 _ -> invalidArgs ["Failed to login user sucessfully"]
