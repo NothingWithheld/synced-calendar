1a5212e4a985d1cf2ee6d0ac0c69fb850cf1c19e
Annie Qiu <annieq2@illinois.edu>
2020-04-26 15:53:17 -0500

    Fix bugs in fetching user

--- server/src/Handler/User.hs
+++ server/src/Handler/User.hs
@@ -12,6 +12,19 @@ strength = 12
 salt :: ByteString
 salt = "3s18bst5ebf"
 
+getUserR :: Handler Value 
+getUserR = do 
+    maybeEmail <- lookupGetParam "email"
+    maybePassword <- lookupGetParam "password"
+    case (maybeEmail, maybePassword) of 
+        (Just email, Just password) -> do 
+            let hashedPass = makePasswordSalt (Import.encodeUtf8 password) (makeSalt salt) strength
+            potentialUsers <- runDB $ selectList [UserEmail ==. email, UserPassword ==. (Import.decodeUtf8 hashedPass)] []
+            case potentialUsers of 
+                [Entity userId _] -> return $ object ["userId" .= userId]
+                _ -> invalidArgs ["Failed to login user sucessfully"]
+        (_, _) -> invalidArgs ["Failed to login user sucessfully"]
+        
 postUserR :: Handler Value 
 postUserR = do 
     maybeEmail <- lookupPostParam "email"
@@ -25,20 +38,7 @@ postUserR = do
                     let registered = True
                     let hashedPass = makePasswordSalt (Import.encodeUtf8 password) (makeSalt salt) strength
                     let user' = User email (Import.decodeUtf8 hashedPass) registered
-                    insertedUser <- runDB $ insertEntity user' 
-                    returnJson insertedUser
+                    Entity userId _ <- runDB $ insertEntity user' 
+                    return $ object ["userId" .= userId]
         (_, _) -> invalidArgs ["Failed to provide email and/or hashed_pass values"]
-
-getUserR :: Handler Value 
-getUserR = do 
-    maybeEmail <- lookupPostParam "email"
-    maybePassword <- lookupPostParam "password"
-    case (maybeEmail, maybePassword) of 
-        (Just email, Just password) -> do 
-            let hashedPass = makePasswordSalt (Import.encodeUtf8 password) (makeSalt salt) strength
-            potentialUsers <- runDB $ selectList [UserEmail ==. email, UserPassword ==. (Import.decodeUtf8 hashedPass)] []
-            case potentialUsers of 
-                [user] -> returnJson user
-                _ -> invalidArgs ["Failed to login user sucessfully"]
-        (_, _) -> invalidArgs ["Failed to login user sucessfully"]
         
